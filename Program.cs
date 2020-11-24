b9ec10ff17f427ffe8c6cda1c6c5107c65c32e44
Кузнецов Александр <aka.kuznec@ya.ru>
2020-11-24 16:59:38 +0300

    Add hosting

--- Program.cs
+++ Program.cs
@@ -1,12 +1,26 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+
 
 namespace StockBot
 {
     class Program
     {
-        static void Main(string[] args)
+        static public IHost host;
+        static Task Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            host = CreateHostBuilder(args)
+                    .UseEnvironment("Development")
+                    .Build();
+
+            
+            return host.RunAsync();
         }
+
+        static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args);
     }
 }
